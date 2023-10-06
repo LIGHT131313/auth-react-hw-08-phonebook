57c958938e9a778c1ef3dbf74a71f57fd2a5a73d
LIGHT131313 <131313light@gmail.com>
2023-10-06 09:50:50 +0300

    v1

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -7,11 +7,13 @@ import { ContactForm } from './ContactForm/ContactForm';
 import { Filter } from './Filter/Filter';
 import { ContactList } from './ContactList/ContactList';
 import { selectStore } from 'redux/selectors';
+import { Loader } from './Loader/Loader';
 
 export const App = () => {
   const dispatch = useDispatch();
-
   const { items, isLoading, error } = useSelector(selectStore);
+  const showLoader = isLoading && !error;
+  const listEmpty = !isLoading && !items.length;
 
   useEffect(() => {
     dispatch(fetchContacts());
@@ -22,15 +24,14 @@ export const App = () => {
       <h1>Phonebook</h1>
       <ContactForm />
       <h2>Contacts</h2>
-      {isLoading && !error && <b>Request in progress...</b>}
-      {items.length > 0 ? (
+      {showLoader && <Loader />}
+      {items.length > 0 && (
         <>
           <Filter />
           <ContactList />
         </>
-      ) : (
-        <p>Contact list is empty</p>
       )}
+      {listEmpty && <p>Contact list is empty</p>}
 
       <GlobalStyle />
     </Layout>
